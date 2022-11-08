f4180b7413d45d676dc501ebcb3312a9f3b925e8
oriolraventosc <oriol.raventos.c@gmail.com>
2022-11-08 02:18:53 +0100

    Add a handler delete

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -7,9 +7,12 @@ const handlers = [
   rest.get(`${apiUrl}robots`, async (request, response, context) => {
     return response(context.status(200), context.json(robotsMock));
   }),
-  rest.get(`${apiUrl}robots/:idRobot`, async (request, response, context) => {
-    return response(context.status(200), context.json(robotsMock));
-  }),
+  rest.delete(
+    `${apiUrl}robots/:idRobot?tokken=${process.env.REACT_APP_TOKEN}`,
+    async (request, response, context) => {
+      return response(context.status(200), context.json(robotsMock));
+    }
+  ),
 ];
 
 export default handlers;
